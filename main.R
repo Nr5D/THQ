e14ed2ceeb9723d71f9176032f689008557d5f9f
Abdul Aziz Nurussadad <56505862+Nr5D@users.noreply.github.com>
2022-12-06 06:39:46 +0700

    Update main.R

--- main.R
+++ main.R
@@ -1,15 +1,25 @@
-# Membaca Daftar Surat dan Ayat
 df <- read.csv("data/forsampling.csv")
 
-# Inisiasi nilai agar while loop berjalan
-n <- 300
+k <- sample(1:6236,1)
 
-while ( n > 280) (
+id_file <- paste0("translations/id/", df$surah[k], "/", df$ayah[k], ".txt" )
+id_text <- readChar(id_file, file.info(id_file)$size)
+
+ar_file <- paste0("surah/", df$surah[k], "/", df$ayah[k], ".txt" )
+ar_text <- readChar(ar_file, file.info(ar_file)$size)
+
+status_details <- paste0(ar_text,
+                         "\n",
+                         "\n",
+                         id_text, " (", df$surah[k], ":",df$ayah[k], ")"
+                         )
+
+while (nchar(status_details) > 280) {
   k <- sample(1:6236,1)
   
   id_file <- paste0("translations/id/", df$surah[k], "/", df$ayah[k], ".txt" )
   id_text <- readChar(id_file, file.info(id_file)$size)
-  
+    
   ar_file <- paste0("surah/", df$surah[k], "/", df$ayah[k], ".txt" )
   ar_text <- readChar(ar_file, file.info(ar_file)$size)
   
@@ -17,13 +27,12 @@ while ( n > 280) (
                            "\n",
                            "\n",
                            id_text, " (", df$surah[k], ":",df$ayah[k], ")"
-                          )
-  n <- nchar(status_details)
-)
+  )
+}
 
 library(rtweet)
 
-token_rm <- rtweet_bot(
+token_rm <- rtweet::rtweet_bot(
   api_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
   api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
