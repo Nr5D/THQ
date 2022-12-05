9e234a9ddd46a2c8ce5335fa1dd5c0fa15e6fb4a
Abdul Aziz Nurussadad <56505862+Nr5D@users.noreply.github.com>
2022-12-05 20:00:46 +0700

    Update main.R

--- main.R
+++ main.R
@@ -5,9 +5,30 @@ k <- sample(1:6236,1)
 id_file <- paste0("translations/id/", df$surah[k], "/", df$ayah[k], ".txt" )
 id_text <- readChar(id_file, file.info(id_file)$size)
 
-
 ar_file <- paste0("surah/", df$surah[k], "/", df$ayah[k], ".txt" )
 ar_text <- readChar(ar_file, file.info(ar_file)$size)
+
+status_details <- paste0(ar_text,
+                         "\n",
+                         "\n",
+                         id_text, " (", df$surah[k], ":",df$ayah[k], ")"
+                         )
+
+while (nchar(status_details) > 280) {
+  k <- sample(1:6236,1)
+  
+  id_file <- paste0("translations/id/", df$surah[k], "/", df$ayah[k], ".txt" )
+  id_text <- readChar(id_file, file.info(id_file)$size)
+    
+  ar_file <- paste0("surah/", df$surah[k], "/", df$ayah[k], ".txt" )
+  ar_text <- readChar(ar_file, file.info(ar_file)$size)
+  
+  status_details <- paste0(ar_text,
+                           "\n",
+                           "\n",
+                           id_text, " (", df$surah[k], ":",df$ayah[k], ")"
+  )
+}
 
 library(rtweet)
 
@@ -16,13 +37,9 @@ token_rm <- rtweet::rtweet_bot(
   api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
   access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
   access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
-)
+  )
 
 post_tweet(
-  status = paste0(ar_text,
-                  "\n",
-                  "\n",
-                  id_text, " (", df$surah[k], ":",df$ayah[k], ")"
-  ),
+  status = status_details,
   token = token_rm               
 )
