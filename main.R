a9593cd755d48518658ec9a892b9d2d3542b4a17
Abdul Aziz Nurussadad <56505862+Nr5D@users.noreply.github.com>
2022-12-05 15:01:07 +0700

    Create main.R

---
+++ main.R
@@ -0,0 +1,28 @@
+df <- read.csv("data/forsampling.csv")
+
+k <- sample(1:6236,1)
+
+id_file <- paste0("surah/", df$surah[k], "/", df$ayah[k], ".txt" )
+id_text <- readChar(id_file, file.info(id_file)$size)
+
+
+ar_file <- paste0("translations/id/", df$surah[k], "/", df$ayah[k], ".txt" )
+ar_text <- readChar(ar_file, file.info(ar_file)$size)
+
+library(rtweet)
+
+token_rm <- rtweet::rtweet_bot(
+  api_key =    Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  api_secret = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+post_tweet(
+  status = paste0(ar_text,
+                  "\n",
+                  "\n",
+                  id_text, " (", df$surah[k], ":",df$ayah[k], ")"
+  ),
+  token = token_rm               
+)
